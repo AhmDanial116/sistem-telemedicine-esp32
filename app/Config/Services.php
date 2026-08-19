9a72088b4cda9c80fbc14d29413f23f7d5595564
Danial <ahmaddanial790@gmail.com>
2026-08-20 01:10:01 +0700

    fixing status pengukuran v3

--- app/Config/Services.php
+++ app/Config/Services.php
@@ -3,6 +3,8 @@
 namespace Config;
 
 use CodeIgniter\Config\BaseService;
+use CodeIgniter\HTTP\IncomingRequest;
+use CodeIgniter\HTTP\UserAgent;
 
 /**
  * Services Configuration file.
@@ -19,6 +21,42 @@ use CodeIgniter\Config\BaseService;
  */
 class Services extends BaseService
 {
+    /**
+     * Override IncomingRequest untuk deployment Railway.
+     *
+     * Pada environment hosting saat ini, request body dari CodeIgniter
+     * dapat tertinggal sebagai string literal "php://input".
+     * Di sini body dibaca langsung sebelum IncomingRequest dibuat,
+     * sehingga JSON POST dari ESP32 masuk ke getBody()/getJSON()
+     * sebagai isi JSON yang sebenarnya.
+     */
+    public static function incomingrequest(
+        ?\Config\App $config = null,
+        bool $getShared = true
+    ) {
+        if ($getShared) {
+            return static::getSharedInstance(
+                'request',
+                $config
+            );
+        }
+
+        $config ??= config(\Config\App::class);
+
+        $rawBody = file_get_contents('php://input');
+
+        if ($rawBody === false) {
+            $rawBody = null;
+        }
+
+        return new IncomingRequest(
+            $config,
+            static::get('uri'),
+            $rawBody,
+            new UserAgent()
+        );
+    }
+
     /*
      * public static function example($getShared = true)
      * {
